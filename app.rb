6fa86c4a39dcee35b59d10ac1cfedc1c71d10763
Michael Fuget <michaelfuget@gmail.com>
2020-12-24 20:06:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
+  
     # code other routes/actions here
+    get '/' do 
+      erb :root 
+    end 
+    get '/new' do 
+      erb "pirates/new".to_sym
+    end 
 
+    post '/pirate' do 
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |x|
+        Ship.new(x)
+      end 
+      
+      @ships = ships.all
+      erb :show
+
+    end 
   end
 end
