3e98294b0dfdbfa575f8be7f49a5fea8da64930e
Michael Fuget <michaelfuget@gmail.com>
2020-12-24 20:24:19 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+    
+  @@all = []  
+    
+  def initialize(params)
+    @name = params[:name]
+    @height = params[:height]
+    @weight = params[:weight]
+    @@all.push(self)
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
